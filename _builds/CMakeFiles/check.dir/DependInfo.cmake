efebd99e2ed274c91063b794337c396ce7f9e863
Syrex333 <ivanchernogorov579@gmail.com>
2025-05-04 21:39:44 +0400

    1

---
+++ _builds/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Syrex333/workspace/projects/lab07/tests/test1.cpp" "/home/user/Syrex333/workspace/projects/lab07/_builds/CMakeFiles/check.dir/tests/test1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/user/projects/hunter/_Base/xxxxxxx/f845a29/cf272be/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/user/Syrex333/workspace/projects/lab07/_builds/CMakeFiles/print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
